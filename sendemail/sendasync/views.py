91efb54348baaddee40a61a5482b332f89bce93f
ngelrojasp <aomarrojasp@gmail.com>
2019-05-12 23:21:42 -0300

    another changes

--- sendemail/sendasync/views.py
+++ sendemail/sendasync/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 #
 from django.core.mail import send_mail
 from django.conf import settings
+from .tasks import add
 
 
 class UserSendMail(generics.ListAPIView):
@@ -11,10 +12,11 @@ class UserSendMail(generics.ListAPIView):
 
     def get(self, request, *args, **kwargs):
         current_user = request.user
+        ans = add.delay(85, 7)
         subject = 'Send from app DJ-CELERY-RABBIT'
         message = 'this is great, because is working its me: '+current_user.username
         email_from = settings.EMAIL_HOST_USER
         recipient_list = ['angel_one@yopmail.com',]
-        send_mail(subject, message, email_from, recipient_list)
-
-        return Response({'data': 'email sent'})
+        resp = send_mail(subject, message, email_from, recipient_list)
+        print(ans)
+        return Response({'data': resp, 'msg': 'email sent'})
--- sendemail/sendemail/wsgi.py
+++ sendemail/sendemail/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sendEmail project.
+WSGI config for sendemail project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sendEmail.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sendemail.settings')
 
 application = get_wsgi_application()
